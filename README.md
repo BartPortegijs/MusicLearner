84474c7666706dc409e14b8c94a4184c0cb4c986
BartPortegijs <b_portegijs@hotmail.com>
2022-09-16 21:22:54 +0200

    First version final additions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MusicLearner
  Using Spotify, learn title and artist of songs
+
+ The goal of this project is to make it easier to really learn titles and artist of a lot of songs in such a way that you will remember it in the future.
+
+ The current status of this project is that the code works, but that it is not yet usable for anyone besides myself.
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from startup import startup
+from startup import startup, addition_name_list, playlist_name_list
 from add_tracks import add_new_songs_to_database, remove_songs_from_database
 from update_songs import update_songs_in_database_from_songlist, check_equality_songlist_playlist_by_name
 from update_playlists import update_playlist
 
 if __name__ == "__main__":
-    addition_name_list = ['check_addition', 'known_addition', 'unknown_addition', 'remove_list']
-    playlist_name_list = ['check', 'learn', 'test', 'first_check']
-
     startup(addition_name_list +playlist_name_list)
     add_new_songs_to_database()
     remove_songs_from_database()
